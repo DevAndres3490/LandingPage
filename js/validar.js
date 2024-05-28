4c7d9f5306dbe2286121f0dba0da1ee690ea6f88
DevAndres3490 <DevAndres3490>
2024-05-28 19:44:26 +0200

    Se ha cambiado la fecha y hora, ahora la hora tambien se recoge desde el campo fecha

--- js/validar.js
+++ js/validar.js
@@ -1,118 +1,19 @@
-(function () {
-  'use strict';
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation');
+  const forms = document.querySelectorAll('.needs-validation')
 
   // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add('was-validated');
-      }, false);
-    });
-
-  const phoneInput = document.getElementById('telefono');
-  const phonePattern = /^\d{6,14}$/;
-
-  phoneInput.addEventListener('input', function () {
-    if (phonePattern.test(phoneInput.value)) {
-      phoneInput.setCustomValidity('');
-    } else {
-      phoneInput.setCustomValidity('Por favor ingrese un número de teléfono válido (6 a 14 dígitos).');
-    }
-  });
-
-  const emailInput = document.getElementById('email');
-  const emailMinLength = 10;
-  const emailMaxLength = 40;
-
-  emailInput.addEventListener('input', function () {
-    const emailValue = emailInput.value;
-    if (emailValue.length >= emailMinLength && emailValue.length <= emailMaxLength) {
-      emailInput.setCustomValidity('');
-    } else {
-      emailInput.setCustomValidity(`Por favor ingrese un correo electrónico válido (${emailMinLength} a ${emailMaxLength} caracteres).`);
-    }
-  });
-
-  const fechaInput = document.getElementById('fecha');
-  const currentDate = new Date();
-  const formattedToday = currentDate.toISOString().split('T')[0];
-  fechaInput.setAttribute('min', formattedToday);
-
-  fechaInput.addEventListener('input', function () {
-    if (new Date(fechaInput.value) < new Date(formattedToday)) {
-      fechaInput.setCustomValidity('Por favor seleccione una fecha válida.');
-    } else {
-      fechaInput.setCustomValidity('');
-    }
-  });
-
-  const nameInput = document.getElementById('name');
-  const apellidoInput = document.getElementById('apellido');
-  const textPattern = /^[A-Za-z\s]+$/;
-
-  function validateTextInput(input, errorMessage) {
-    if (textPattern.test(input.value)) {
-      input.setCustomValidity('');
-    } else {
-      input.setCustomValidity(errorMessage);
-    }
-  }
-
-  nameInput.addEventListener('input', function () {
-    validateTextInput(nameInput, 'Por favor ingrese un nombre válido sin números.');
-  });
-
-  apellidoInput.addEventListener('input', function () {
-    validateTextInput(apellidoInput, 'Por favor ingrese un apellido válido sin números.');
-  });
-
-  function actualizarHorasDisponibles() {
-    const fechaValue = document.getElementById('fecha').value;
-    const selectedDate = new Date(fechaValue);
-    const currentHour = currentDate.getHours();
-    const currentDay = selectedDate.getDay();
-    let limitHour;
-
-    // Determinar el límite de horas según el día de la semana
-    if (currentDay >= 1 && currentDay <= 5) { // De lunes a viernes
-      limitHour = 19;
-    } else if (currentDay === 6) { // Sábado
-      limitHour = 15;
-    } else { // Domingo
-      limitHour = 0; // No se permiten reservas los domingos
-    }
-
-    const horaSelect = document.getElementById('hora');
-    horaSelect.innerHTML = ''; // Limpiar opciones anteriores
-
-    // Si la fecha es hoy, limitar las horas disponibles a partir de la hora actual
-    if (selectedDate.toDateString() === currentDate.toDateString()) {
-      for (let i = Math.max(currentHour + 1, 9); i <= limitHour && i <= 19; i++) {
-        const option = document.createElement('option');
-        option.text = `${i}:00`; // Formato de hora HH:00
-        horaSelect.add(option);
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
       }
-    } else {
-      // Si no es hoy, mostrar todas las horas disponibles dentro del límite
-      for (let i = 9; i <= limitHour && i <= 19; i++) {
-        const option = document.createElement('option');
-        option.text = `${i}:00`; // Formato de hora HH:00
-        horaSelect.add(option);
-      }
-    }
-  }
 
-  // Agregar un evento change al input de fecha para actualizar las horas disponibles
-  document.getElementById('fecha').addEventListener('change', actualizarHorasDisponibles);
-
-  // Llamar a la función al cargar la página para mostrar las horas disponibles basadas en la fecha inicial
-  actualizarHorasDisponibles();
-})();
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
